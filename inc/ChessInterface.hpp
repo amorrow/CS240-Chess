4e6830229c06326b3b86b42a9f956b163d2745bb
Andrew Morrow <morrowa@betelgeuse.local>
2009-11-16 23:07:45 -0700

    Added skeleton ChessInterface header.

---
+++ inc/ChessInterface.hpp
@@ -0,0 +1,14 @@
+/* ChessInterface
+ * Facade class to simplify GUI access to chess methods.
+ */
+
+#ifndef _CS240_CHESS_INTERFACE_HPP
+#define _CS240_CHESS_INTERFACE_HPP
+
+class ChessInterface
+{
+public:
+	static void startNewGame();
+};
+
+#endif
