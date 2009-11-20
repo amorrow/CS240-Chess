6a0f01100f7051ee2bfdbc9d5a8ab0245fd0439b
Andrew David Morrow <morrowa@amoeba.cs.byu.edu>
2009-11-19 20:06:16 -0700

    Added ignore directives. Added library makefile.

---
+++ library.mk
@@ -0,0 +1,15 @@
+#library.mk is a makefile generated using the compiler to determine dependencies\n
+lib/obj/ChessGuiBoardCell.o: lib/src/ChessGuiBoardCell.cpp \
+  lib/inc/ChessGuiBoardCell.h lib/inc/ChessGuiDefines.h \
+  lib/inc/ChessGuiImages.h
+lib/obj/ChessGuiBoard.o: lib/src/ChessGuiBoard.cpp \
+  lib/inc/ChessGuiBoard.h lib/inc/ChessGuiDefines.h \
+  lib/inc/ChessGuiBoardCell.h lib/inc/ChessGui.h lib/inc/ChessGuiBoard.h \
+  lib/inc/ChessGuiImages.h
+lib/obj/ChessGui.o: lib/src/ChessGui.cpp lib/inc/ChessGui.h \
+  lib/inc/ChessGuiDefines.h lib/inc/ChessGuiBoard.h \
+  lib/inc/ChessGuiBoardCell.h lib/inc/ChessGuiImages.h \
+  lib/inc/SelectDialog.h
+lib/obj/ChessGuiImages.o: lib/src/ChessGuiImages.cpp \
+  lib/inc/ChessGuiImages.h lib/inc/ChessGuiDefines.h lib/inc/Inline.h
+lib/obj/SelectDialog.o: lib/src/SelectDialog.cpp lib/inc/SelectDialog.h
