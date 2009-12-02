afcb87082184996276ce0c5a18325f9790f0a9da
Andrew David Morrow <morrowa@amoeba.cs.byu.edu>
2009-12-02 16:45:49 -0700

    Implemented Rook movements.

--- src/Rook.cpp
+++ src/Rook.cpp
@@ -3,6 +3,7 @@
 #include "Rook.h"
 #include "ChessDefinitions.h"
 #include <glib.h>
+#include "Board.h"
 
 using namespace std;
 
@@ -13,7 +14,83 @@ set<Location> Rook::validMoves(Location current, Board& board) const
 {
 	g_debug("Rook asked for its moves.");
 	set<Location> moves;
-	moves.insert(Location(0,1));
+	//look to the right until we run into the edge or another piece
+	for (int col = current.column() + 1; col < BOARD_NUM_COLS; col++)
+	{
+		Location next(current.row(), col);
+		if (board.at(next) != NULL)
+		{
+			if (board.at(next)->color() != this->color())
+			{
+				// enemy piece; make it available for the taking
+				moves.insert(next);
+			}
+			break; // no moves further to the right
+		}
+		else
+		{
+			// no piece there
+			moves.insert(next);
+		}
+	}
+
+	for (int col = current.column() - 1; col >= 0; col--)
+	{
+		Location next(current.row(), col);
+		if (board.at(next) != NULL)
+		{
+			if (board.at(next)->color() != this->color())
+			{
+				// enemy piece; make it available for the taking
+				moves.insert(next);
+			}
+			break; // no moves further to the left
+		}
+		else
+		{
+			// no piece there
+			moves.insert(next);
+		}
+	}
+
+	for (int row = current.row() - 1; row >= 0; row--)
+	{
+		Location next(row, current.column());
+		if (board.at(next) != NULL)
+		{
+			if (board.at(next)->color() != this->color())
+			{
+				// enemy piece; make it available for the taking
+				moves.insert(next);
+			}
+			break; // no moves further to the left
+		}
+		else
+		{
+			// no piece there
+			moves.insert(next);
+		}
+	}
+
+	for (int row = current.row() + 1; row < BOARD_NUM_ROWS; row++)
+	{
+		Location next(row, current.column());
+		if (board.at(next) != NULL)
+		{
+			if (board.at(next)->color() != this->color())
+			{
+				// enemy piece; make it available for the taking
+				moves.insert(next);
+			}
+			break; // no moves further to the left
+		}
+		else
+		{
+			// no piece there
+			moves.insert(next);
+		}
+	}
+
 	return moves;
 }
 
