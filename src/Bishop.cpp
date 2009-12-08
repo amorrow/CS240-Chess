6dc297f964722a753fb5bc01abe572b62e42f635
Andrew David Morrow <morrowa@rowing.cs.byu.edu>
2009-12-08 11:07:00 -0700

    Fixed problems with checkstyle for bishop.

--- src/Bishop.cpp
+++ src/Bishop.cpp
@@ -8,92 +8,44 @@ using namespace std;
 Bishop::Bishop(ChessColor color) : Piece(ChessPieceTypeBishop, color)
 {}
 
+void addMoves(Location current, Board& board, set<Location>& moves, int rowDiff, int colDiff, ChessColor ownColor)
+{
+	Location next = Location(current.row() + rowDiff, current.column() + colDiff);
+	while(true)
+	{
+		if (!next.isValid())
+			break;
+		if (board.at(next) != NULL)
+		{
+			if (board.at(next)->color() != ownColor)
+			{
+				moves.insert(next);
+			}
+			break;
+		}
+		else
+		{
+			moves.insert(next);
+		}
+		next = Location(next.row() + rowDiff, next.column() + colDiff);
+	}
+}
+
 set<Location> Bishop::validMoves(Location current, Board& board) const
 {
 	set<Location> moves;
 	//work along the up & right diagonal
-	Location next(current.row()+1, current.column()+1);
-	while(true)
-	{
-		if (!next.isValid())
-			break;
-		if (board.at(next) != NULL)
-		{
-			if (board.at(next)->color() != this->color())
-			{
-				moves.insert(next);
-			}
-			break;
-		}
-		else
-		{
-			moves.insert(next);
-		}
-		next = Location(next.row() + 1, next.column() + 1);
-	}
+	addMoves(current, board, moves, 1, 1, this->color());
+	
 	
 	// up and to the left
-	next = Location(current.row() + 1, current.column() - 1);
-	while(true)
-	{
-		if (!next.isValid())
-			break;
-		if (board.at(next) != NULL)
-		{
-			if (board.at(next)->color() != this->color())
-			{
-				moves.insert(next);
-			}
-			break;
-		}
-		else
-		{
-			moves.insert(next);
-		}
-		next = Location(next.row() + 1, next.column() - 1);
-	}
+	addMoves(current, board, moves, 1, -1, this->color());
 
 	// down and to the left
-	next = Location(current.row() - 1, current.column() - 1);
-	while(true)
-	{
-		if (!next.isValid())
-			break;
-		if (board.at(next) != NULL)
-		{
-			if (board.at(next)->color() != this->color())
-			{
-				moves.insert(next);
-			}
-			break;
-		}
-		else
-		{
-			moves.insert(next);
-		}
-		next = Location(next.row() - 1, next.column() - 1);
-	}
+	addMoves(current, board, moves, -1, -1, this->color());
 
 	// down and to the right
-	next = Location(current.row() - 1, current.column() + 1);
-	while(true)
-	{
-		if (!next.isValid())
-			break;
-		if (board.at(next) != NULL)
-		{
-			if (board.at(next)->color() != this->color())
-			{
-				moves.insert(next);
-			}
-			break;
-		}
-		else
-		{
-			moves.insert(next);
-		}
-		next = Location(next.row() - 1, next.column() + 1);
-	}
+	addMoves(current, board, moves, -1, 1, this->color());
 
 	return moves;
 }
